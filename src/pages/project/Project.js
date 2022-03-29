e652f5720335c53ca1f569efcfdbd4694a32b14e
Dietmar <eis0687.dietmar@gmail.com>
2022-03-29 22:13:29 +0200

    comment output in console works

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -5,6 +5,7 @@ import { useDocument } from '../../hooks/useDocument';
 
 // styles
 import './Project.css'
+import ProjectComments from './ProjectComments';
 import ProjectSummary from './ProjectSummary';
 
 const Project = () => {
@@ -21,6 +22,7 @@ const Project = () => {
   return (
     <div className='project-details'>
       <ProjectSummary project = { document } />
+      <ProjectComments />
     </div>
   );
 }
--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,3 +1,4 @@
+import { v4 as uuid } from 'uuid';
 import { useState } from 'react';
 
 //firebase
@@ -7,8 +8,36 @@ import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 
 const ProjectComments = () => {
+  const [newComment, setNewComment] = useState('');
+  const { user } = useAuthContext();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const commentToAdd = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      content: newComment,
+      createdAt: timestamp.fromDate(new Date()),
+      id: uuid()
+    }
+    console.log(commentToAdd)
+  }
   return (
-    <div></div>
+    <div className='project-comments'>
+      <h4>Project Comments</h4>
+      <form className='add-comment' onSubmit={handleSubmit}>
+        <label>
+          <span>Add new comment:</span>
+          <textarea
+            required
+            onChange={ e => setNewComment(e.target.value) }
+            value={newComment}
+          />
+        </label>
+        <button className="btn">Add Comment</button>
+      </form>
+    </div>
   );
 }
  
